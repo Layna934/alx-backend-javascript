6dc8645f0dddb6e90e253d50589cac65a934ebf5
Precious <eseprecious934@gmail.com>
2024-10-09 02:28:58 +0000

    get students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Function to get array of student info
+ *
+ * @returns[{id: Number, firstName: String, location: String}]
+ */
+
+export default function getListStudents() {
+	return [
+		{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+		{id: 2, firstName: 'James', location: 'Columbia'},
+		{id: 3, firstName: 'Serena', location: 'San Francisco'}
+	];
+}
